7f1eaeaec871b1ea399e2ee8aaf39ec18573c6ca
SofianeLasri <alasri250@gmail.com>
2024-03-11 13:57:57 +0100

    Réplicage des modifs du prof 1

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import {gql} from 'graphql-tag'
-
-const doctorsData = [
-  {
-    id: "1",
-    name: 'Samia Mekame',
-    speciality: 'OPHTALMOLOGIST',
-  },
-  {
-    id: "2",
-    name: 'Catherine Bedoy',
-    speciality: 'PSYCHOLOGIST',
-  },
-];
+import {gql} from 'graphql-tag';
+import { resolvers } from "./resolvers.js";
 
 const typeDefs = gql`
 type Doctor {
   id: String
   name: String
-  speciality: SPECIALITY
+  speciality: Speciality
   addresses: Address
 }
 
@@ -27,93 +15,21 @@ type Address {
   streetName: String
 }
 
-enum SPECIALITY {
+enum Speciality {
   PSYCHOLOGIST
   OPHTALMOLOGIST
 }
 
 type Query {
   doctor(id: ID!): Doctor
-  doctors(specialities: [SPECIALITY!]): [Doctor]
+  doctors(specialities: [Speciality!]): [Doctor]
   add(number1: Float!, number2: Float!): Float!
   subtract(number1: Float!, number2: Float!): Float!
   multiply(number1: Float!, number2: Float!): Float!
   divide(number1: Float!, number2: Float!): Float!
-  closestColor(color: String!): String!
+  closestColor(hexa: String!): String!
 }
 `
-
-const colors: string[] = ["#FF5733", "#33FF57", "#3357FF"];
-
-const closestColor = (color: string) => {
-  if(!color.startsWith("#")) color = "#" + color;
-
-  const colorValues = color
-      .slice(1)
-      .match(/.{2}/g)
-      ?.map((hex) => parseInt(hex, 16));
-
-  if (!colorValues) {
-    throw new Error("Invalid color");
-  }
-
-  const [r, g, b] = colorValues;
-
-  const colorDistances = colors.map((c) => {
-    const [cr, cg, cb] = c
-        .slice(1)
-        .match(/.{2}/g)
-        ?.map((hex) => parseInt(hex, 16)) as number[];
-
-    return Math.sqrt(
-        Math.pow(r - cr, 2) + Math.pow(g - cg, 2) + Math.pow(b - cb, 2)
-    );
-  });
-
-  const closestColorIndex = colorDistances.indexOf(
-      Math.min(...colorDistances)
-  );
-
-  return colors[closestColorIndex];
-}
-
-const resolvers = {
-  Query: {
-    doctors: (parent, args, context, info) => {
-      return doctorsData
-    },
-    doctor: (parent, args, context, info) => {
-      const {id} = args
-      return doctorsData.find(d => d.id === id)
-    },
-    add: (parent, args, context, info) => {
-      const {number1, number2} = args
-      return number1 + number2
-    },
-    subtract: (parent, args, context, info) => {
-      const {number1, number2} = args
-      return number1 - number2
-    },
-    multiply: (parent, args, context, info) => {
-      const {number1, number2} = args
-      return number1 * number2
-    },
-    divide: (parent, args, context, info) => {
-      const {number1, number2} = args
-      return number1 / number2
-    },
-    closestColor: (parent, args, context, info) => {
-      const {color} = args
-      return closestColor(color)
-    }
-  },
-  Doctor: {
-    addresses: (parent, args, context, info) => {
-      console.log(parent)
-      return {streetName: `${parent.id} street`}
-    }
-  }
-}
 
 const server = new ApolloServer({
   typeDefs,
